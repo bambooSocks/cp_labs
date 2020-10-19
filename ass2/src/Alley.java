79dc1a5630b0a65a0bf7275137c272ceb2d25bac
Matej <majtan.matej@gmail.com>
2020-10-20 00:30:31 +0200

    problem 1 and 2 ... kinda 3 not done yet

--- ass2/src/Alley.java
+++ ass2/src/Alley.java
@@ -6,30 +6,51 @@
 
 public class Alley {
 
-    Semaphore mutex;
+    Semaphore up, down, alley;
+    boolean dir = false;
 
     public Alley() {
-        mutex = new Semaphore(1);
+        up = new Semaphore(1);
+        down = new Semaphore(1);
+        alley = new Semaphore(4);
     }
 
     /* Determine whether pos is right before alley is entered */
     public boolean atEntry(Pos pos) {
-        return false;
+        return pos.equals(new Pos(10, 0)) || pos.equals(new Pos(1, 1)) || pos.equals(new Pos(2, 1)) ;
     }
 
     /* Determine whether pos is right after alley is left */
     public boolean atExit(Pos pos) {
-        return false;
+        return pos.equals(new Pos(9, 1)) || pos.equals(new Pos(0, 0));
     }
 
     /* Block until car no. may enter alley */
     public void enter(int no) throws InterruptedException {
-        mutex.P();
+        if (no != 0) {
+            boolean curDir = no < 5;
+            if (curDir == dir) {
+                alley.P();
+            } else {
+                if (curDir) {
+                    down.P();
+                } else {
+                    up.P();
+                }
+                dir = curDir;
+            }
+        }
     }
 
     /* Register that car no. has left the alley */
     public void leave(int no) {
-        mutex.V();
+        alley.V();
+
+        if (no < 5) {
+            down.V();
+        } else {
+            up.V();
+        }
     }
 
 }
--- ass2/src/Field.java
+++ ass2/src/Field.java
@@ -4,20 +4,30 @@
 
 //Hans Henrik Lovengreen     Oct 2, 2020
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Field {
 
-    public Field() {
+    Semaphore accessMap;
+    Map<Pos, Semaphore> positionSemaphore;
 
+    public Field() {
+        positionSemaphore = new HashMap<>();
+        accessMap = new Semaphore(1);
     }
 
     /* Block until car no. may safely enter tile at pos */
     public void enter(int no, Pos pos) throws InterruptedException {
-
+        accessMap.P();
+        positionSemaphore.putIfAbsent(pos, new Semaphore(1));
+        accessMap.V();
+        positionSemaphore.get(pos).P();
     }
 
     /* Release tile at position pos */
-    public void leave(Pos pos) {
-
+    public void leave(Pos pos) throws InterruptedException {
+        positionSemaphore.get(pos).V();
     }
 
 }
